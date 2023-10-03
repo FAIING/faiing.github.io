a13a8866dc8ad702100fabe99ca8595587cd7deb
FAIING <1916785778@qq.com>
2023-10-03 13:04:52 +0000

    更新 UTC 2023-10-03 Tuesday 13:04:52 Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,54 @@
+var posts=["posts/132e.html","posts/5365.html","posts/1236.html","posts/2a6b.html","posts/2f2d.html","posts/307a.html","posts/a43b.html","posts/2f50.html","posts/e48d.html","posts/da01.html","posts/ca0f.html","posts/d50a.html","posts/9a66.html","posts/89da.html","posts/606d.html","posts/cc75.html","posts/7770.html","posts/8b30.html","posts/8f83.html","posts/a277.html","posts/4eb3.html","posts/bd97.html","posts/dfba.html","posts/e6db.html","posts/d68b.html","posts/995e.html","posts/5515.html","posts/7d9f.html","posts/6996.html","posts/1fda.html","posts/d58f.html","posts/4ed7.html","posts/43bc.html","posts/bfbd.html","posts/1848.html","posts/9252.html","posts/7a3f.html","posts/4287.html","posts/30de.html","posts/b37e.html","posts/4313.html","posts/9e86.html","posts/6fc8.html","posts/a17d.html","posts/52d2.html","posts/a54.html","posts/3525.html","posts/456f.html","posts/370d.html","posts/2fc2.html","posts/9d14.html","posts/2514.html","posts/10c.html","posts/128e.html","posts/97c1.html","posts/2f9e.html","posts/cc0f.html","posts/bd48.html","posts/d01c.html","posts/daaf.html","posts/4dda.html","posts/fa6.html","posts/565d.html","posts/f059.html","posts/d0e7.html","posts/f662.html","posts/51cf.html","posts/48f7.html","posts/1e53.html","posts/5a80.html","posts/d6b.html","posts/ed8a.html","posts/c4a7.html","posts/e311.html","posts/3665.html","posts/8dbe.html","posts/a71d.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[{"name":"安知鱼","hundredSuffix":"","link":"https://anzhiy.cn/","avatar":"https://cdn.jsdelivr.net/gh/FAIING/imgbed@main/blog/202305271534543.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-blog@1.1.6/img/post/common/anzhiy.cn.jpg"},{"name":"张洪Heo","hundredSuffix":"","link":"https://blog.zhheo.com/","avatar":"https://bu.dusays.com/2022/12/28/63ac2812183aa.png","descr":"分享设计与科技生活","siteshot":"https://img.yhq.zone/cover/202305271554675.png"},{"name":"远陌Eamo","hundredSuffix":"","link":"https://blog.eamo.top/","avatar":"/img/favicon.ico","descr":"快快乐乐，简简单单","siteshot":"https://img.yhq.zone/cover/202305271529544.png"},{"name":"安知鱼","hundredSuffix":"","link":"https://anzhiy.cn/","avatar":"https://cdn.jsdelivr.net/gh/FAIING/imgbed@main/blog/202305271534543.jpg","descr":"生活明朗，万物可爱"},{"name":"Adil","hundredSuffix":"","link":"https://blog.adil.com.cn","avatar":"https://tucdn.wpon.cn/2023/04/06/15dbbb2004b44.jpg","descr":"BW/HANA顾问,BI工程师,数据分析师,数据科学家。","topimg":"https://tucdn.wpon.cn/2023/04/06/126fba2e88c54.png"},{"name":"Akilar","hundredSuffix":"","link":"https://akilar.top","avatar":"https://bu.dusays.com/2022/08/14/62f87864834ae.png","topimg":"/img/acrylic.png","descr":"欢迎光临糖果屋","rss":"/atom.xml"},{"name":"张洪Heo","hundredSuffix":"","link":"https://blog.zhheo.com","avatar":"https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png","topimg":"/img/acrylic.png","descr":"分享设计与科技生活","rss":"/rss.xml"},{"name":"Leonus","hundredSuffix":"","link":"https://blog.leonus.cn","avatar":"https://q1.qlogo.cn/g?b=qq&nk=553344777&s=5","topimg":"/img/acrylic.png","descr":"进一寸有进一寸的欢喜。","tag":"技术","rss":"/atom.xml"},{"name":"轻笑Chuckle","hundredSuffix":"","link":"https://www.chuckle.top","avatar":"https://cdn1.tianli0.top/npm/chuckle-js-css/chuckle/head.webp","topimg":"/img/acrylic.png","descr":"宁静致远,倾尘轻笑","tag":"技术","rss":"/atom.xml"},{"name":"Ariasakaの小窝","hundredSuffix":"","link":"https://yisous.xyz","avatar":"https://bu.dusays.com/2022/10/22/6352dfd1f3fa3.png","topimg":"/img/acrylic.png","descr":"人有悲欢离合 月有阴晴圆缺","tag":"技术","rss":"/atom.xml"},{"name":"阿豪AHao","hundredSuffix":"","link":"https://ahao.ah.cn","avatar":"https://image.ahao.ah.cn/3cccc50598660ee1cece6e10be840f2.jpg","descr":"人间值得，未来可期","siteshot":"https://image.ahao.ah.cn/b4f3987b256a963bd96cf48d75068e5.png"},{"name":"Tiamo","hundredSuffix":"","link":"https://xiaoadai.com","avatar":"https://s1.ax1x.com/2023/05/28/p9LLwod.jpg","descr":"失于急躁终究有失水准","siteshot":"https://s1.ax1x.com/2023/05/28/p9LLddH.png"},{"name":"Gtwxxh-小小窝","hundredSuffix":"","link":"https://blog.gtwxxh.cn","avatar":"https://cdn.staticaly.com/gh/gtwxxh666/pic@main/1/user.JPG","descr":"扣首问路，码梦为生","siteshot":"https://cdn.staticaly.com/gh/gtwxxh666/pic@main/1/202303051810903.png"},{"name":"小志IT知识库","hundredSuffix":"","link":"https://www.czfq99.cn","avatar":"https://www.czfq99.cn/upload/2022/08/12.jpg","descr":"一个专注于安全领域的全栈博客"},{"name":"王貔貅","hundredSuffix":"","link":"https://blog.wpixiu.cn/","avatar":"https://picture.wpixiu.cn/vSIsvO.jpg","descr":"日就月将,学有缉熙于光明","siteshot":"https://picture.wpixiu.cn/34O6IS.jpg"},{"name":"米粒个人博客","hundredSuffix":"","link":"https://luomengguo.top/","avatar":"https://img01.anzhiy.cn/useruploads/128/2023/06/03/647b2d3308108.webp","descr":"追梦少年,敢干敢搏"},{"name":"摸鱼星球","hundredSuffix":"","link":"https://moyustar.cn","avatar":"https://moyustar.cn/icon.png","descr":"摸鱼星球，这是一个分享后端知识与代码的博客。私人若彩虹，遇见方知有，相信你可以在这里找到对你有用的知识和教程"},{"name":"海底捞里没有鱼","hundredSuffix":"","link":"https://smileszh.cn","avatar":"https://bu.dusays.com/2023/06/10/648499e1299d0.jpg","descr":"平安喜乐，万事胜意","siteshot":"https://bu.dusays.com/2023/06/23/64948455a7895.png"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
